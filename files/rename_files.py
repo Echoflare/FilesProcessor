e9f67bf524db26aed7af814464b3de3fb147497b
Echoflare <144500846+Echoflare@users.noreply.github.com>
2024-01-08 01:00:53 +0800

    Create rename_files.py

---
+++ files/rename_files.py
@@ -0,0 +1,33 @@
+import os
+import re
+import sys
+
+import argparse
+
+parser = argparse.ArgumentParser(description='文件重命名脚本设置项')
+
+parser.add_argument('-d', '--directory-path', default='.', help='目录路径')
+parser.add_argument('-n', '--name-startswith', default='file', help='文件前缀')
+
+args = parser.parse_args()
+
+directory_path = args.directory_path
+name_startswith = args.name_startswith
+
+def rename_files(directory):
+    file_files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and f != os.path.basename(__file__)]
+    
+    for index, file_name in enumerate(file_files, start=1):
+        new_name = f'{name_startswith}_{index}.{file_name.split(".")[-1]}'
+        
+        while os.path.exists(os.path.join(directory, new_name)):
+            index += 1
+            new_name = f'{name_start}_{index}.{file_name.split(".")[-1]}'
+        
+        old_path = os.path.join(directory, file_name)
+        new_path = os.path.join(directory, new_name)
+        
+        os.rename(old_path, new_path)
+        print(f'重命名: {file_name} -> {new_name}')
+
+rename_files(directory_path)
