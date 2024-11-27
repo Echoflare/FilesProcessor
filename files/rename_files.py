0f77eff8c5ccb55fedb7e7b2f8129dfdff5c1330
Echoflare <144500846+Echoflare@users.noreply.github.com>
2024-11-28 07:23:34 +0800

    Add files via upload

--- files/rename_files.py
+++ files/rename_files.py
@@ -8,16 +8,18 @@ parser = argparse.ArgumentParser(description='文件重命名脚本设置项')
 
 parser.add_argument('-d', '--directory-path', default='.', help='目录路径')
 parser.add_argument('-n', '--name-startswith', default='file', help='文件前缀')
+parser.add_argument('-s', '--start_num', type=int, default=1, help='序号起始值')
 
 args = parser.parse_args()
 
 directory_path = args.directory_path
 name_startswith = args.name_startswith
+start_num = args.start_num
 
 def rename_files(directory):
     file_files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and f != os.path.basename(__file__)]
     
-    for index, file_name in enumerate(file_files, start=1):
+    for index, file_name in enumerate(file_files, start=start_num):
         new_name = f'{name_startswith}_{index}.{file_name.split(".")[-1]}'
         
         while os.path.exists(os.path.join(directory, new_name)):
